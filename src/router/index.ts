e6b868ff94b9f464182ecf374ff4029a83747edb
dassieu.damien <dassieu.damien@gmail.com>
2022-08-27 19:41:48 +0200

    Pattern front #1

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Place from '../views/Place.vue';
 import Login from '../views/Login.vue';
 import User from '../views/User.vue';
+import Pattern from '../views/Pattern.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/user',
       name: 'user',
       component: User
+    },
+    {
+      path: '/pattern',
+      name: 'pattern',
+      component: Pattern
     }
   ]
 })
--- src/views/Pattern.vue
+++ src/views/Pattern.vue
@@ -1,34 +1,14 @@
-<script setup lang="ts">
-    import router from '@/router/index';
-    import { useColorsStore } from '@/stores/colors.js';
-    import { useTimerStore } from '@/stores/timer.js';
-    import { useMapStore } from '@/stores/map.js';
-    import { usePixelStore } from '@/stores/pixel.js';
-    import axios from 'axios';
+<script setup lang='ts'>
     import $ from 'jquery';
-    import io from "socket.io-client";
-    import { refreshToken } from '@/auth.js';
     import panzoom from 'panzoom';
-    // https://github.com/thecodealer/vue-panzoom
-
-    const TOKEN = localStorage.getItem('ACCESS_TOKEN');
-    const HEADERS = {
-                'Accept': '*/*',
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'Authorization': `Bearer ${localStorage.getItem('ACCESS_TOKEN')}`
-            };
-
-
-    const timerSts = useTimerStore();
-    const mapSts = useMapStore();
-    const colorsSts = useColorsStore();
-    var selector: {x: number, y: number};
-    var colorSelected: string = 'none';
-    var isFree = true;
+    import { useColorsStore } from '@/stores/colors.js';
 
     var canvas: HTMLCanvasElement;
     var ctx: CanvasRenderingContext2D;
+    var selector: {x: number, y: number};
+    var colorSelected: string = 'none';
+    var isFree = true;
+    const colorsSts = useColorsStore();
 
     function rgbToHex(r: number, g: number, b: number) {
         if (r > 255 || g > 255 || b > 255)
@@ -53,7 +33,7 @@
                 pixel.color[1],
                 pixel.color[2]
             )).slice(-6);
-            setPixel(pixel);
+            setPixelBorder(pixel);
         }
     }
 
@@ -84,22 +64,38 @@
         ctx.fillRect(x+8, y+9, 2, 1);
     }
 
-    // Document ready
-    $(function() {
 
+    function setPixelBorder(pixel: any) {
+        let x = pixel['coord_x'];
+        let y = pixel['coord_y'];
+        let color = pixel['color'];
+
+        ctx.fillStyle = color;
+        ctx.fillRect(x+0, y+0, 1, 2);
+        ctx.fillRect(x+0, y+0, 2, 1);
+        ctx.fillRect(x+9, y+0, 1, 2);
+        ctx.fillRect(x+8, y+0, 2, 1);
+        ctx.fillRect(x+0, y+9, 2, 1);
+        ctx.fillRect(x+0, y+8, 1, 2);
+        ctx.fillRect(x+9, y+8, 1, 2);
+        ctx.fillRect(x+8, y+9, 2, 1);
+    }
+
+    $(function() {
         canvas = <HTMLCanvasElement>$('#place')[0];
         ctx = canvas.getContext('2d');
 
-        /* TEMP */
+        /*  TEMP    */
         canvas.width = 2000;
         canvas.height = 2000;
         ctx.fillStyle = 'white';
         ctx.fillRect(0, 0, 2000, 2000);
-        ctx.fillStyle = 'green';
-        ctx.fillRect(1990, 1000, 10, 10);
-        ctx.fillRect(0, 1000, 10, 10);
-        ctx.fillRect(0, 0, 10, 10);
-
+        for(let i=0; i<200; i++) {
+            for(let j=0; j<200; j++) {
+                ctx.fillStyle = 'black';
+                ctx.fillRect((i*10)+4, (j*10)+4, 2, 2);
+            }
+        }
         colorsSts.addColor({name: 'red', hex: '#FF0000'});
         colorsSts.addColor({name: 'green', hex: '#00FF00'});
         colorsSts.addColor({name: 'blue', hex: '#0000FF'});
@@ -108,48 +104,11 @@
         colorsSts.addColor({name: 'pink', hex: '#e835d9'});
         colorsSts.addColor({name: 'brown', hex: '#702c04'});
         colorsSts.addColor({name: 'orange', hex: '#f07f07'});
-        /* END TEMP */
-
-        // INIT SOCKET CONNEXION
-        const socket = io(`http://${import.meta.env.VITE_APP_BACKEND_URL}`, {
-            extraHeaders: HEADERS
-        });
-
-
-        // GET MAP + USER SPECS
-        axios.get(`http://${import.meta.env.VITE_APP_BACKEND_API_URL}/user/game/all`, {
-            headers: HEADERS,
-            method: 'GET',
-        }).then(res => {
-
-            // USER INFOS
-            timerSts.setTimer(res.data.timer);
-            mapSts.setWidth(res.data.width);
-            for(let color of res.data.colors) {
-                colorsSts.addColor({
-                    name: color.split(':')[0],
-                    hex: color.split(':')[1]
-                });
-            }
-
-            // MAP
-            const width = res.data.width;
-            const map = res.data.map;
-            setMap(width, map);
-
-        }).catch(async err => {
-            console.log(err);
-            if(!await refreshToken()) {
-                // router.push('/login');
-            }
-        });
-
-
-        // UPDATE PIXEL
-        socket.on('pixel', pixel => {
-            setPixel(pixel);
-        });
-
+        ctx.fillStyle = 'green';
+        ctx.fillRect(1990, 1000, 10, 10);
+        ctx.fillRect(0, 1000, 10, 10);
+        ctx.fillRect(0, 0, 10, 10);
+        /*  END TEMP    */
 
         // PANZOOM
         const instance = panzoom(canvas, {
@@ -162,6 +121,7 @@
                 if(colorSelected !== 'none') {
                     $('#place-pixel').addClass('place-pixel-button');
                 }
+                $('#remove-pixel').addClass('place-pixel-button');
                 return false;
             }
         });
@@ -216,69 +176,24 @@
                     if(colorSelected !== 'none') {
                         $('#place-pixel').addClass('place-pixel-button');
                     }
+                    $('#remove-pixel').addClass('place-pixel-button');
                 } else {
                     setSelector(x, y);
                     if(! (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) ) {
                         isFree = true;
                         $('#place-pixel').removeClass('place-pixel-button');
+                        $('#remove-pixel').removeClass('place-pixel-button');
                     } else {
                         if(colorSelected !== 'none') {
                             $('#place-pixel').addClass('place-pixel-button');
                         }
+                        $('#remove-pixel').addClass('place-pixel-button');
                     }
                 }
             }
         });
 
     });
-
-
-    function setPixel(pixel: any) {
-        let x = pixel['coord_x'];
-        let y = pixel['coord_y'];
-        let color = pixel['color'];
-
-        ctx.fillStyle = color;
-        ctx.fillRect(x+0, y+0, 1, 2);
-        ctx.fillRect(x+0, y+0, 2, 1);
-        ctx.fillRect(x+9, y+0, 1, 2);
-        ctx.fillRect(x+8, y+0, 2, 1);
-        ctx.fillRect(x+0, y+9, 2, 1);
-        ctx.fillRect(x+0, y+8, 1, 2);
-        ctx.fillRect(x+9, y+8, 1, 2);
-        ctx.fillRect(x+8, y+9, 2, 1);
-    }
-
-
-    function setMap(width: number, map: [] | any[]) {
-        canvas.width = width * 10;
-        canvas.height = width * 10;
-
-        map.forEach(pixel => {
-            setPixel(pixel);
-        });
-    }
-
-
-    function getSinglePixel(coord_x: number, coord_y: number) {
-        const pixelSts = usePixelStore();
-        axios.get(`http://${import.meta.env.VITE_APP_BACKEND_API_URL}/pixel?coord_x=${coord_x}&coord_y=${coord_y}`, {
-            headers: HEADERS,
-            method: 'GET',
-        }).then(res => {
-            pixelSts.setPixel({
-                coord_x: res.data['coord_x'],
-                coord_y: res.data['coord_y'],
-                color: res.data['color'],
-                user: res.data['user'].split('.')[1],
-                date: res.data['data']
-            });
-        }).catch(async err => {
-            if(!await refreshToken()) {
-                router.push('/login');
-            }
-        });
-    }
 
 
     function setColor(name: string) {
@@ -291,63 +206,77 @@
 
     function placePixel() {
         if(colorSelected !== 'none' && selector) {
-            if($("#set-perm").is(':checked')) {
-                console.log('perm');
-            } else {
-                console.log('pas perm');
-            }
             console.log("place pixel at " + selector.x + " " + selector.y);
             console.log(colorSelected);
             colorSelected = 'none';
             $('#place-pixel').removeClass('place-pixel-button');
+            $('#remove-pixel').removeClass('place-pixel-button');
         }
     }
 
+    function setPatternPixel(x: number, y: number) {
+        ctx.fillStyle = 'white';
+        ctx.fillRect(x, y, 10, 10);
+        ctx.fillStyle = 'black';
+        ctx.fillRect(x+4, y+4, 2, 2);
+        
+        //  Border selector
+        ctx.fillRect(x+0, y+0, 1, 2);
+        ctx.fillRect(x+0, y+0, 2, 1);
+        ctx.fillRect(x+9, y+0, 1, 2);
+        ctx.fillRect(x+8, y+0, 2, 1);
+        ctx.fillRect(x+0, y+9, 2, 1);
+        ctx.fillRect(x+0, y+8, 1, 2);
+        ctx.fillRect(x+9, y+8, 1, 2);
+        ctx.fillRect(x+8, y+9, 2, 1);
+    }
+
+    function removePixel() {
+        console.log("remove pixel at " + selector.x + " " + selector.y);
+        setPatternPixel(selector.x, selector.y);
+        $('#place-pixel').removeClass('place-pixel-button');
+        $('#remove-pixel').removeClass('place-pixel-button');
+    }
 
 </script>
 
 <template>
 
-    <div id="parent-canvas">
-        <canvas id="place"></canvas>
-    </div>
-    <div id="select-pixel">
-        <div id="select-color-container">
-            <div v-for="color in colorsSts.colors" :key="color.name">
-                <div :id="`select-${color.name}`" class="select-color"
-                 :style="{background: color.hex}" :title="color.name"
-                 @click="setColor(color.name)"></div>
+    <div id="screen">
+        <div id="parent-canvas">
+            <canvas id="place"></canvas>
+        </div>
+        <div id="select-pixel">
+            <div id="select-color-container">
+                <div v-for="color in colorsSts.colors" :key="color.name">
+                    <div :id="`select-${color.name}`" class="select-color"
+                    :style="{background: color.hex}" :title="color.name"
+                    @click="setColor(color.name)"></div>
+                </div>
             </div>
-        </div>
-        <p @click="placePixel" id="place-pixel" class="mb-0 px-2 pb-1 mt-2">Place pixel</p>
-        <div id="set-perm-div">
-            <input title="Set permanent" type="checkbox" id="set-perm" value="perm">
+            <p @click="placePixel" id="place-pixel" class="pixel-button mx-2 mb-0 px-2 pb-1 mt-2">Place pixel</p>
+            <p @click="removePixel" id="remove-pixel" class="pixel-button mx-2 mb-0 px-2 pb-1 mt-2">Remove pixel</p>
         </div>
     </div>
-
+    
 </template>
 
 <style scoped>
 
-#set-perm {
-    width: 18px;
-    height: 18px;
-}
-
-#set-perm-div {
+.pixel-button {
     position: relative;
-    float: right;
-    top: 13px;
+    display: inline-block;
+    color: white;
+    background-color: #2c3e50;
 }
 
 #place-pixel {
-    position: relative;
-    display: inline-block;
     border-radius: 3px;
-    left: 50%;
-    transform: translateX(-50%);
-    color: white;
-    background-color: #2c3e50;
+    cursor: not-allowed;
+}
+
+#remove-pixel {
+    border-radius: 3px;
     cursor: not-allowed;
 }
 
@@ -387,6 +316,13 @@
 
 #place {
     image-rendering: pixelated;
+}
+
+#screen {
+    overflow: hidden; /* Hide scrollbars */
+    position: relative;
+    width: 100%;
+    min-height: 100vh;
 }
 
 </style>
