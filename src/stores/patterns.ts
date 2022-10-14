ead4f7a887a94695df19fc995eacdaf2d72b3f0e
dassieu.damien <dassieu.damien@gmail.com>
2022-10-14 11:02:09 +0200

    Pattern name regex + modal on delete

--- src/stores/patterns.ts
+++ src/stores/patterns.ts
@@ -1,23 +1,33 @@
 import { defineStore } from 'pinia'
 
-export type Patterns = { self: [], bind: [] };
+export type Patterns = { self: [], bind: [], wantDeleteName: string, wantDeleteId: string };
 
 export const usePatternsStore = defineStore({
   id: 'patterns',
   state: () => ({
     _patterns: {
         self: [],
-        bind: []
+        bind: [],
+        wantDeleteName: '',
+        wantDeleteId: ''
     } as Patterns
   }),
   getters: {
     self: (state) => state._patterns.self,
-    bind: (state) => state._patterns.bind
+    bind: (state) => state._patterns.bind,
+    deleteName: (state) => state._patterns.wantDeleteName,
+    deleteId: (state) => state._patterns.wantDeleteId
   },
   actions: {
     setPatterns(patterns: {self: [], bind: []}) {
       this._patterns.self = patterns.self;
       this._patterns.bind = patterns.bind;
+    },
+    setDeleteName(wantDelete: string) {
+      this._patterns.wantDeleteName = wantDelete;
+    },
+    setDeleteId(wantDelete: string) {
+      this._patterns.wantDeleteId = wantDelete;
     }
   }
 })
--- src/views/PatternList.vue
+++ src/views/PatternList.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import router from '@/router/index';
 import $ from 'jquery';
+import { Modal } from 'bootstrap';
 import axios from 'axios';
 import { usePatternsStore } from '@/stores/patterns.js';
 import http from '@/router/http';
@@ -33,17 +34,27 @@ function onLink(e: any | Event) {
     navigator.clipboard.writeText(location.host+'?pattern='+e.currentTarget.id.split('/')[1]);
 }
 
-function onRemove(e: any | Event) {
-    let id = e.currentTarget.id;
-    http.delete(`http://${import.meta.env.VITE_APP_BACKEND_API_URL}/pattern/${id.split('/')[1]}`, {
+function onRemove(e: any | Event, type: string, name: string) {
+    const id = e.currentTarget.id.split('/')[1];
+    patternsSts.setDeleteName(name);
+    patternsSts.setDeleteId(id);
+    if (type == 'self') {
+        const modal = new Modal(document.getElementById('deleteModal') ?? '', {});
+        modal.show();
+    } else {
+        doDelete();
+    }
+    e.stopPropagation();
+    e.stopImmediatePropagation();
+}
+
+function doDelete() {
+    http.delete(`http://${import.meta.env.VITE_APP_BACKEND_API_URL}/pattern/${patternsSts.deleteId}`, {
         headers: HEADERS,
         method: 'DELETE',
     }).then(res => {
-        document.getElementById('pattern/'+id.split('/')[1])?.remove();
-        document.getElementById('pattern/'+id.split('/')[1])?.remove();
+        document.getElementById('pattern/'+patternsSts.deleteId)?.remove();
     });
-    e.stopPropagation();
-    e.stopImmediatePropagation();
 }
 
 function goToPattern(e: any | Event) {
@@ -52,22 +63,44 @@ function goToPattern(e: any | Event) {
 }
 
 function createPattern() {
+    const modal = new Modal(document.getElementById('deleteModal') ?? '', {});
+    modal.show();
     const name = $('#name').val()?.toString();
-    http.post(`http://${import.meta.env.VITE_APP_BACKEND_API_URL}/pattern`, {
-        patternName: name
-    }, {
-        headers: HEADERS,
-        method: 'POST',
-    }).then(res => {
-        console.log(res.data)
-        const id = res.data.patternId;
-        router.push(`/pattern/${id}`);
-    });
+    const regex = /^[^\/|^\n]+$/;
+    if (name?.match(regex)) {
+        http.post(`http://${import.meta.env.VITE_APP_BACKEND_API_URL}/pattern`, {
+            patternName: name
+        }, {
+            headers: HEADERS,
+            method: 'POST',
+        }).then(res => {
+            const id = res.data.patternId;
+            router.push(`/pattern/${id}`);
+        });
+    }
 }
 
 </script>
 
 <template>
+
+    <div id="deleteModal" class="modal fade" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Delete {{ patternsSts.wantDeleteName }}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p>Are you sure you want to delete <strong>{{ patternsSts.wantDeleteName }}</strong> ?</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button @click="doDelete" type="button" class="btn btn-danger" data-bs-dismiss="modal">Delete</button>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <div class="position-fixed bottom-0 end-0 p-3" style="z-index: 11">
         <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
@@ -86,7 +119,7 @@ function createPattern() {
                 <li v-for="pattern in patternsSts.self" :key="pattern.patternId" :id="'pattern/'+pattern.patternId">
                     <router-link :to="'/pattern/'+pattern.patternId" class="me-3">{{ pattern.name }}</router-link>
                     <svg @click="onLink" class="pointer link me-2" :id="'copy/'+pattern.patternId" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M18 6v-6h-18v18h6v6h18v-18h-6zm-16 10v-14h14v4h-10v10h-4z"/></svg>
-                    <svg @click="onRemove" class="pointer remove" :id="'remove/'+pattern.patternId" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M19 24h-14c-1.104 0-2-.896-2-2v-16h18v16c0 1.104-.896 2-2 2m-9-14c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6 0c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6-5h-20v-2h6v-1.5c0-.827.673-1.5 1.5-1.5h5c.825 0 1.5.671 1.5 1.5v1.5h6v2zm-12-2h4v-1h-4v1z"/></svg>
+                    <svg @click="onRemove(e, 'self', pattern.name)" class="pointer remove" :id="'remove/'+pattern.patternId" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M19 24h-14c-1.104 0-2-.896-2-2v-16h18v16c0 1.104-.896 2-2 2m-9-14c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6 0c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6-5h-20v-2h6v-1.5c0-.827.673-1.5 1.5-1.5h5c.825 0 1.5.671 1.5 1.5v1.5h6v2zm-12-2h4v-1h-4v1z"/></svg>
                     <svg @click="goToPattern" width="30" height="30" class="ms-2 pointer" :id="'goto/'+pattern.patternId" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m13.022 14.999v3.251c0 .412.335.75.752.75.188 0 .375-.071.518-.206 1.775-1.685 4.945-4.692 6.396-6.069.2-.189.312-.452.312-.725 0-.274-.112-.536-.312-.725-1.451-1.377-4.621-4.385-6.396-6.068-.143-.136-.33-.207-.518-.207-.417 0-.752.337-.752.75v3.251h-9.02c-.531 0-1.002.47-1.002 1v3.998c0 .53.471 1 1.002 1zm1.5-4.498v-3.008l4.751 4.507-4.751 4.507v-3.008h-10.022v-2.998z" fill-rule="nonzero"/></svg>
                 </li>
             </ul>
@@ -95,7 +128,7 @@ function createPattern() {
                 <li v-for="pattern in patternsSts.bind" :key="pattern.patternId" :id="'pattern/'+pattern.patternId">
                     <router-link :to="'/pattern/'+pattern.patternId" class="me-3">{{ pattern.name }}</router-link>
                     <svg @click="onLink" class="pointer link me-2" :id="'copy/'+pattern.patternId" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M18 6v-6h-18v18h6v6h18v-18h-6zm-16 10v-14h14v4h-10v10h-4z"/></svg>
-                    <svg @click="onRemove" class="pointer remove" :id="'remove/'+pattern.patternId" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M19 24h-14c-1.104 0-2-.896-2-2v-16h18v16c0 1.104-.896 2-2 2m-9-14c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6 0c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6-5h-20v-2h6v-1.5c0-.827.673-1.5 1.5-1.5h5c.825 0 1.5.671 1.5 1.5v1.5h6v2zm-12-2h4v-1h-4v1z"/></svg>
+                    <svg @click="onRemove(e, 'bind', pattern.name)" class="pointer remove" :id="'remove/'+pattern.patternId" width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M19 24h-14c-1.104 0-2-.896-2-2v-16h18v16c0 1.104-.896 2-2 2m-9-14c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6 0c0-.552-.448-1-1-1s-1 .448-1 1v9c0 .552.448 1 1 1s1-.448 1-1v-9zm6-5h-20v-2h6v-1.5c0-.827.673-1.5 1.5-1.5h5c.825 0 1.5.671 1.5 1.5v1.5h6v2zm-12-2h4v-1h-4v1z"/></svg>
                     <svg @click="goToPattern" width="30" height="30" class="ms-2 pointer" :id="'goto/'+pattern.patternId" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m13.022 14.999v3.251c0 .412.335.75.752.75.188 0 .375-.071.518-.206 1.775-1.685 4.945-4.692 6.396-6.069.2-.189.312-.452.312-.725 0-.274-.112-.536-.312-.725-1.451-1.377-4.621-4.385-6.396-6.068-.143-.136-.33-.207-.518-.207-.417 0-.752.337-.752.75v3.251h-9.02c-.531 0-1.002.47-1.002 1v3.998c0 .53.471 1 1.002 1zm1.5-4.498v-3.008l4.751 4.507-4.751 4.507v-3.008h-10.022v-2.998z" fill-rule="nonzero"/></svg>
                 </li>
             </ul>
@@ -105,7 +138,7 @@ function createPattern() {
                     <label for="name" class="form-label">Pattern name</label>
                     <input type="text" class="form-control" id="name" name="name">
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="submit" class="btn btn-primary">Create</button>
             </form>
         </div>
     </div>
